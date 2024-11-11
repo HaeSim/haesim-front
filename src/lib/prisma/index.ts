0ff678714eb4fd7f5c4e4f295bc3b1a9669cc7a3
Dean <cheoljin.an@lotte.net>
2024-11-11 17:03:34 +0900

    fix: prisma build 연동 코드 추가

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,5 +1,14 @@
+// src/lib/prisma/index.ts
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export default prisma;
+const client = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = client;
+}
+
+export default client;
